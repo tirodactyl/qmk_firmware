49461a380f22c126a1fd9ab39c8d368014732226
Tirso Peguero <tito.peguero@gmail.com>
2017-05-09 23:32:17 -0700

    add 3x3 macropad config

---
+++ keyboards/handwired/threebythree/threebythree.h
@@ -0,0 +1,17 @@
+#ifndef THREEBYTHREE_H
+#define THREEBYTHREE_H
+
+#include "quantum.h"
+
+#define KEYMAP( \
+	k00, k01, k02, \
+	k03, k04, k05, \
+	k06, k07, k08 \
+) \
+{ \
+	{ k00, k01, k02 }, \
+	{ k03, k04, k05 }, \
+	{ k06, k07, k08 } \
+}
+
+#endif
