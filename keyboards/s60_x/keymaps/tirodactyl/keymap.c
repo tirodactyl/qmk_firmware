ca29dab60d67d8a4cfcee81f0bfb82de333d7b6d
Tirso Peguero <tito.peguero@gmail.com>
2018-01-31 21:39:17 -0800

    fix keymapping

--- keyboards/s60_x/keymaps/tirodactyl/keymap.c
+++ keyboards/s60_x/keymaps/tirodactyl/keymap.c
@@ -14,12 +14,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* |Fn0 |Gui |Alt |         Space          |Alt |Gui |App |Ctrl|
 	* `-----------------------------------------------------------'
 	*/
-    LEGACY_KEYMAP(
-		ESC,  1,    2,    3,    4,    5,    6,    7,    8,    9,    0, MINS,  EQL, BSPC, BSPC, \
-		TAB,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P, LBRC, RBRC, BSLS, \
-		LCTL, A,    S,    D,    F,    G,    H,    J,    K,    L, SCLN, QUOT, NUHS, ENT,  \
-		LSFT, NUBS, Z,    X,    C,    V,    B,    N,    M, COMM,  DOT, SLSH,   NO, RSFT, NO, \
-		FN1,  LGUI, LALT,            KC_SPACE,                   RALT, RGUI,  APP,  RCTL),
+    KEYMAP(
+		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSPC, KC_BSPC, \
+		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS, \
+		MOD_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  \
+		MOD_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_NO, MOD_RSFT, KC_NO, \
+		KC_FN0,  MOD_LGUI, MOD_LALT,            KC_SPACE,                   MOD_RALT, MOD_RGUI,  KC_APP,  MOD_RCTL),
 
 	/* Layout 1: Function Layer
 	* ,-----------------------------------------------------------.
@@ -34,12 +34,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* |    |    |    |                        |    |    |    |    |
 	* `-----------------------------------------------------------'
 	*/
-    LEGACY_KEYMAP(
-		GRV,  F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,  TRNS, DEL,  \
-		TRNS, MPRV, MPLY, MNXT, MSTP, TRNS, HOME, PGDN, PGUP, END,  TRNS, SLCK, PAUS, INS,  \
-		TRNS, VOLD, MUTE, VOLU, TRNS, TRNS, LEFT, DOWN, UP,   RGHT, TRNS, TRNS, PENT, \
-		TRNS, TRNS, PSCR, FN1,  FN2,  FN3,  CALC, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, CAPS, TRNS, \
-		TRNS, TRNS, TRNS,             TRNS,                         TRNS, TRNS, TRNS, TRNS),
+    KEYMAP(
+		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_DEL,  \
+		KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_SLCK, KC_PAUS, KC_INS,  \
+		KC_TRNS, KC_VOLD, KC_MUTE, KC_VOLU, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_PENT, \
+		KC_TRNS, KC_TRNS, KC_PSCR, KC_FN1,  KC_FN2,  KC_FN3,  KC_CALC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_CAPS, KC_TRNS, \
+		KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS,                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 };
 
 /*
